cbf809eafde50cacd4d4d3139729873a97bcbe38
Jesus Lopez <jesusmlopezf@me.com>
2016-06-14 20:56:42 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+var req = require('request');
+var SerialPort = require('serialport').SerialPort;
+
+var serial = new SerialPort("/dev/ttyACM0", {
+	baudrate: 9600
+}, false);
+
+function main(){
+	serial.open(function(err) {
+		if(err)
+			console.log(err.message);
+		req('http://infinite-brushlands-67485.herokuapp.com/same', function(error, response, body) {
+			if(!error && response.statusCode == 200) {
+				var jsonObj = JSON.parse(body);
+				process(jsonObj);	
+			}
+		});
+	});
+}
+
+main();
+
+function process(object){
+	object.forEach(function(o) {
+		console.log((Math.floor(o['angle1']*(1023/Math.PI))+660)+","+Math.floor(o['angle2']*(1023/Math.PI))+","+(Math.floor(o['angle3']*(1023/Math.PI))+460));
+		var stop = new Date().getTime();
+		while(new Date().getTime() < stop + 5000);
+		console.log('D'+(Math.floor(o['angle1']*(1023/Math.PI))+660)+' C'+(Math.floor(o['angle2']*(1023/Math.PI))+0)+' B'+(Math.floor(o['angle3']*(1023/Math.PI))+460)+' ');
+		serial.write('D'+(Math.floor(o['angle1']*(1023/Math.PI))+660)+' C'+(Math.floor(o['angle2']*(1023/Math.PI))+0)+' B'+(Math.floor(o['angle3']*(1023/Math.PI))+460)+' ', function(err,bytesWritten){
+			if(err) {
+				console.log('Error: ',err.message);
+			}
+		});
+		serial.write('A0 ');
+	});
+}
